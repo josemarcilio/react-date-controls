b55d922067b481a717708b47295c72ca24d92b58
Marcilio Leite <marcilio.leite@zbra.com.br>
2021-05-07 20:45:09 -0300

    Added a shift in daysOfWeek to enable the first day in the week

--- src/components/calendar/specs/useCalendarHeader.spec.ts
+++ src/components/calendar/specs/useCalendarHeader.spec.ts
@@ -1,10 +1,51 @@
 import { renderHook } from "@testing-library/react-hooks"
+import {
+  isFriday,
+  isMonday,
+  isSaturday,
+  isSunday,
+  isThursday,
+  isTuesday,
+  isWednesday,
+} from "date-fns"
 import { useCalendarHeader } from "../useCalendarHeader"
+
+function assertDaysOfWeek(daysOfWeek: Date[], firstDayOfWeek = 0) {
+  const validators = [
+    isSunday,
+    isMonday,
+    isTuesday,
+    isWednesday,
+    isThursday,
+    isFriday,
+    isSaturday,
+  ]
+
+  const daysOfWeekValidators = validators
+    .slice(firstDayOfWeek, validators.length)
+    .concat(validators.slice(0, firstDayOfWeek))
+
+  for (let i = 0; i < daysOfWeek.length; i++) {
+    expect(daysOfWeekValidators[i](daysOfWeek[i])).toBeTruthy()
+  }
+}
 
 test("should generate correct number of days of week", () => {
   const daysInWeek = 7
+  let firstDayOfWeek = 0
 
-  const { result } = renderHook(() => useCalendarHeader())
+  const { result, rerender } = renderHook(() =>
+    useCalendarHeader(firstDayOfWeek)
+  )
 
   expect(result.current.daysOfWeek).toHaveLength(daysInWeek)
+
+  assertDaysOfWeek(result.current.daysOfWeek)
+
+  for (let i = 0; i < daysInWeek; i++) {
+    firstDayOfWeek = i
+    rerender()
+
+    assertDaysOfWeek(result.current.daysOfWeek, firstDayOfWeek)
+  }
 })
--- src/components/calendar/useCalendarHeader.ts
+++ src/components/calendar/useCalendarHeader.ts
@@ -1,5 +1,30 @@
-export function useCalendarHeader() {
-  const daysOfWeek = [...Array(7).keys()]
+import {
+  nextFriday,
+  nextMonday,
+  nextSaturday,
+  nextSunday,
+  nextThursday,
+  nextTuesday,
+  nextWednesday,
+  startOfWeek,
+} from "date-fns"
+
+export function useCalendarHeader(firstDayOfWeek = 0) {
+  const today = startOfWeek(new Date())
+
+  const days = [
+    nextSunday(today),
+    nextMonday(today),
+    nextTuesday(today),
+    nextWednesday(today),
+    nextThursday(today),
+    nextFriday(today),
+    nextSaturday(today),
+  ]
+
+  const daysOfWeek = days
+    .slice(firstDayOfWeek, days.length)
+    .concat(days.slice(0, firstDayOfWeek))
 
   return { daysOfWeek }
 }
