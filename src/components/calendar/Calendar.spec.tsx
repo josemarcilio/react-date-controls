0e041bad14d7a1a5997c605ffa29b61b7ecc0bc3
Marcilio Leite <marcilio.leite@zbra.com.br>
2021-05-07 01:48:00 -0300

    Added more tests for calendar

--- src/components/calendar/Calendar.spec.tsx
+++ src/components/calendar/Calendar.spec.tsx
@@ -1,9 +1,10 @@
 import "@testing-library/jest-dom/extend-expect"
 import { render } from "@testing-library/react"
+import { getDaysInMonth } from "date-fns"
 import React from "react"
 import { Calendar } from "./Calendar"
 
-test("should render calendar component", () => {
+test("should render calendar component", async () => {
   const locales = "default"
   const initialMonth = new Date(2021, 4, 1)
   const selectedDates = [
@@ -12,19 +13,36 @@ test("should render calendar component", () => {
     new Date(2021, 4, 3),
   ]
 
-  const { getByTestId } = render(
+  const { getByTestId, findAllByRole } = render(
     <Calendar initialMonth={initialMonth} initialSelectedDates={selectedDates}>
-      {({ monthName }) => {
-        return (
-          <>
-            <div data-testid="month">{monthName}</div>
-          </>
-        )
-      }}
+      {({ monthName, dates }) => (
+        <>
+          <div data-testid="month">{monthName}</div>
+
+          <ul role="list">
+            {dates.map((date) => (
+              <Calendar.Date
+                key={date.value.getDate()}
+                date={date}
+                locales={locales}
+              >
+                {({ dayOfMonth, weekdayLong }) => (
+                  <li role="listitem">
+                    {dayOfMonth} - {weekdayLong}
+                  </li>
+                )}
+              </Calendar.Date>
+            ))}
+          </ul>
+        </>
+      )}
     </Calendar>
   )
 
   expect(getByTestId("month")).toHaveTextContent(
     initialMonth.toLocaleString(locales, { month: "long" })
   )
+
+  const dates = await findAllByRole("listitem")
+  expect(dates).toHaveLength(getDaysInMonth(initialMonth))
 })
--- src/components/calendar/Calendar.tsx
+++ src/components/calendar/Calendar.tsx
@@ -1,19 +1,22 @@
+import { getDate, getDay } from "date-fns"
 import React, { ReactNode } from "react"
 import { CalendarProvider } from "./CalendarContext"
-import { useCalendar, UseCalendarType } from "./useCalendar"
+import type { UseCalendarType } from "./useCalendar"
+import { useCalendar } from "./useCalendar"
+import type { CalendarDate } from "./useCalendarDates"
 
 export type CalendarType = UseCalendarType & {
   monthName: string
 }
 
 export type CalendarProps = {
-  children: (props: CalendarType) => ReactNode
+  children: (value: CalendarType) => ReactNode
   initialMonth: Date
   initialSelectedDates: Date[]
   locales?: string
 }
 
-export function Calendar({
+function Calendar({
   children,
   initialMonth,
   initialSelectedDates,
@@ -30,3 +33,46 @@ export function Calendar({
 
   return <CalendarProvider>{children(args)}</CalendarProvider>
 }
+
+export type CalendarDateType = CalendarDate & {
+  dayOfMonth: number
+  weekday: number
+  weekdayLong: string
+  weekdayShort: string
+  weekdayNarrow: string
+}
+
+export type CalendarDateProps = {
+  children: (value: CalendarDateType) => ReactNode
+  date: CalendarDate
+  locales?: string
+}
+
+Calendar.Date = function ({
+  children,
+  date,
+  locales = "default",
+}: CalendarDateProps) {
+  const dayOfMonth = getDate(date.value)
+  const weekday = getDay(date.value)
+  const weekdayLong = date.value.toLocaleString(locales, { weekday: "long" })
+  const weekdayShort = date.value.toLocaleString(locales, { weekday: "short" })
+  const weekdayNarrow = date.value.toLocaleString(locales, {
+    weekday: "narrow",
+  })
+
+  return (
+    <>
+      {children({
+        ...date,
+        dayOfMonth,
+        weekday,
+        weekdayLong,
+        weekdayShort,
+        weekdayNarrow,
+      })}
+    </>
+  )
+}
+
+export { Calendar }
