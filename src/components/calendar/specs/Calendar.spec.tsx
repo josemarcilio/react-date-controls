aa2a173f98334d3773d1f7e445e35dd7080bbb3e
Marcilio Leite <marcilio.leite@zbra.com.br>
2021-05-10 18:11:43 -0300

    Calendar spec updated

--- src/components/calendar/specs/Calendar.spec.tsx
+++ src/components/calendar/specs/Calendar.spec.tsx
@@ -1,47 +1,10 @@
 import "@testing-library/jest-dom/extend-expect"
-import { render, screen } from "@testing-library/react"
+import { render } from "@testing-library/react"
 import { getDate, getDay } from "date-fns"
 import React from "react"
 import { Calendar } from "../Calendar"
 
-// test("should render Calendar component", async () => {
-//   const locales = "default"
-//   const initialMonth = new Date(2021, 4, 1)
-
-//   render(
-//     <Calendar initialMonth={initialMonth} initialSelectedDates={[]}>
-//       {() => {
-//         return <></>
-//       }}
-//     </Calendar>
-//   )
-// })
-
 test("should render CalendarDate component", () => {
-  const initialMonth = new Date(2021, 4, 1)
-  const locales = "default"
-
-  render(
-    <Calendar
-      initialMonth={initialMonth}
-      initialSelectedDates={[]}
-      locales={locales}
-    >
-      <Calendar.Date>
-        {({ date, dayOfMonth, weekday }) => {
-          expect(dayOfMonth).toEqual(getDate(date.value))
-          expect(weekday).toEqual(getDay(date.value))
-
-          return <></>
-        }}
-      </Calendar.Date>
-    </Calendar>
-  )
-
-  screen.debug()
-})
-
-test("should render CalendarWeekDay component", () => {
   const initialMonth = new Date(2021, 4, 1)
   const locales = "default"
 
@@ -67,8 +30,15 @@ test("should render CalendarWeekDay component", () => {
           return <></>
         }}
       </Calendar.Weekday>
+
+      <Calendar.Date>
+        {({ date, dayOfMonth, weekday }) => {
+          expect(dayOfMonth).toEqual(getDate(date.value))
+          expect(weekday).toEqual(getDay(date.value))
+
+          return <></>
+        }}
+      </Calendar.Date>
     </Calendar>
   )
-
-  screen.debug()
 })
