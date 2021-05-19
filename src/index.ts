6efbbfb4b961b67ca5aef7381cd2eaf2a0cd4ed3
Marcilio Leite <marcilio.leite@zbra.com.br>
2021-05-18 22:08:56 -0300

    Added types to index

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+export { Calendar } from "./calendar/Calendar";
+export type { CalendarProps } from "./calendar/Calendar";
+export type { CalendarDateProps } from "./calendar/CalendarDate";
+export type { CalendarDatesProps } from "./calendar/CalendarDates";
+export type { CalendarHeaderProps } from "./calendar/CalendarHeader";
+export type { CalendarWeekdayProps } from "./calendar/CalendarWeekday";
+
