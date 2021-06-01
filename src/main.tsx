a915b1bbcfab377f1a6fab763df4211436b7d50b
Marcilio Leite <marcilio.leite@zbra.com.br>
2021-06-01 10:21:12 -0300

    Changes in tsconfig and file excluded

--- src/main.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react"
-import ReactDOM from "react-dom"
-
-ReactDOM.render(
-  <React.StrictMode>
-    <p>Hello Vite + React!</p>
-  </React.StrictMode>,
-  document.getElementById("root")
-)
