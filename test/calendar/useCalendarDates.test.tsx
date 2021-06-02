2eae29c8b81c3699cd3179893c7e70d59e6a5315
Marcilio Leite <marcilio.leite@zbra.com.br>
2021-06-02 19:59:54 -0300

    Update in useCalendarDates.test.tsx

--- test/calendar/useCalendarDates.test.tsx
+++ test/calendar/useCalendarDates.test.tsx
@@ -2,11 +2,13 @@ import { renderHook } from "@testing-library/react-hooks"
 import { useCalendarDates } from "../../src/calendar/useCalendarDates"
 
 describe("useCalendarDates", () => {
-  test("should start with selected dates", () => {
+  test("should return the correct dates", () => {
     const month = new Date(2021, 4, 1)
 
     const { result } = renderHook(() => useCalendarDates({ month }))
+    const uniqueDates = new Set(result.current)
 
     expect(result.current.length).toEqual(31)
+    expect(uniqueDates.size).toEqual(result.current.length)
   })
 })
